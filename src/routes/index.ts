efdb16bbfa937ced1dd0e0c9aa5302301e962b0e
alexroses <alexandrehmrosas@gmail.com>
2024-08-14 22:41:52 -0300

    base criada

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import *  as PageController from '../controllers/pageController';
+
+
+const router = Router();
+
+router.get('/', PageController.home);
+
+export default router;
